56e49ef61df61758732e5fb973d64be846ac3577
HASHIM BOLAJI BELLO <bolaji1050@gmail.com>
2024-04-13 11:00:21 +0300

    utility functions added

--- app.py
+++ app.py
@@ -1,8 +1,3 @@
-import cv2 as cv
-from src.pipeline.stage_02_detection import DetectionPipeline
+from card_reader.logger import logger
 
-img = cv.imread('data/1.jpg')
-img = cv.cvtColor(img, cv.COLOR_BGR2RGB)
-
-obj = DetectionPipeline()
-obj.main(img)
+logger.info('Hello World')
