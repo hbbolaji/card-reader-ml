bc762aee7bf345d8fae93712ded8be7b423928f0
HASHIM BOLAJI BELLO <bolaji1050@gmail.com>
2024-04-15 16:30:25 +0300

    prediction pipeline added

--- app.py
+++ app.py
@@ -1,14 +1,43 @@
-from card_reader.pipeline.ingestion_pipeline import DataIngestionPipeline
-from card_reader.pipeline.validation_pipeline import ValidationPipeline
-from card_reader.pipeline.training_pipeline import TrainingPipeline 
+import io
+import os
+from PIL import Image
+from flask import Flask, jsonify, request
+from flask_cors import CORS, cross_origin
+
+from card_reader.pipeline.layout_prediction_pipeline import LayoutPredictionPipeline
+from card_reader.logger import logger
+
+ALLOWED_EXTENSIONS = {'jpg', 'jpeg'}
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+os.putenv('LANG', 'en_US.UTF-8')
+os.putenv('LC_ALL', 'en_US.UTF-8')
+
+app = Flask(__name__)
+CORS(app)
+
+@app.route('/predict', methods=['POST'])
+@cross_origin()
+def predict():
+  predictor = LayoutPredictionPipeline()
+  if request.method == 'POST':
+    file = request.files['file']
+    if file is None or file.filename == '':
+      return jsonify({'error': 'no file'})
+    if not allowed_file(file.filename):
+      return jsonify({'error': 'format not supported'})
+    
+    try:
+      predictor.get_card(image=Image.open(file))
+      result = predictor.predict()
+      return jsonify(result)
+    except Exception as e:
+      logger.error(e)
+      return jsonify({'error': 'Error in the application'})
 
 
-obj = DataIngestionPipeline()
-obj.main()
-
-obj = ValidationPipeline()
-obj.main()
-
-obj = TrainingPipeline()
-obj.main()
-
+if __name__ == '__main__':
+  app.run(host='0.0.0.0', port=8080)
---
+++ card_reader/pipeline/layout_prediction_pipeline.py
@@ -0,0 +1,65 @@
+from ultralytics import YOLO
+import cv2 as cv
+import numpy as np
+from pathlib import Path
+from easyocr import Reader
+
+from card_reader.utils.common import read_yaml
+
+# image = cv.imread('data/8.jpg')
+# image = cv.cvtColor(image, cv.COLOR_BGR2RGB)
+
+
+class LayoutPredictionPipeline:
+  def __init__(self) -> None:
+    self.model = YOLO('model/best.pt')
+    self.classes = read_yaml(Path('model/data.yaml')).names
+    self.reader = Reader(['en'], detector="DB", gpu=False)
+
+  def layout_extraction(self, image, box):
+      x1, y1, x2, y2 = box.xyxy[0]
+      x1, y1, x2, y2 = int(x1), int(y1), int(x2), int(y2)
+      return image[y1:y2, x1:x2]
+  
+  def text_extraction(self, output):
+    text = []
+    for item in output:
+      text.append(item[1])
+    return text
+  
+  def get_class(self, id):
+    return self.classes[int(id)]
+  
+  def is_excluded(self, class_name):
+    exclude = ['mykad', 'mykid', 'mykad_icon', 'mykid_icon', 'flag', 'heading']
+    return class_name in exclude
+
+  
+  def get_card(self, image):
+    image = np.array(image)
+    result = self.model.predict(image)
+    for res in result:
+      boxes = res.boxes
+      for box in boxes:
+        if self.get_class(box.cls) == 'mykad':
+          self.card = self.layout_extraction(image, box)
+  
+  def predict(self):
+    response = {}
+    result = self.model(self.card)
+    for res in result:
+      boxes = res.boxes
+      for box in boxes:
+        class_name = self.get_class(box.cls)
+        if not self.is_excluded(class_name):
+          layout = self.layout_extraction(self.card, box)
+          output = self.reader.readtext(layout)
+          text = self.text_extraction(output=output)
+          response[class_name] = text
+    
+    return response
+
+    
+
+# predictor = LayoutPredictionPipeline()
+# predictor.predict(image)
